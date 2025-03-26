d6f39bad7c6cfc538078a8b73cb1c246807a8435
Sowmya Thottambeti <sowmya6598@gmail.com>
2025-03-25 22:16:35 -0400

    Update for 2025 season

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ player_id = playerid_lookup('chisholm', 'jazz')
 #   name_last  name_first  key_mlbam  key_retro  key_bbref  key_fangraphs  mlb_played_first  mlb_played_last
 #   chisholm   jazz        665862     chisj001   chishja01  20454          2020.0            2024.0
 
-data = statcast_batter('2020-01-01', '2024-10-30', 665862)
+data = statcast_batter('2020-01-01', '2025-12-12', 665862)
 
 home_runs = data[data['events'] == 'home_run'].copy()
 
